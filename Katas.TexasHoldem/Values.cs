f1ee4f5f913a161ac0e32cc975f8e1491029db69
Dennis Somerville <dennis.somerville@gmail.com>
2013-08-09 23:05:21 -0400

    Changed to constants.

--- Katas.TexasHoldem/Values.cs
+++ Katas.TexasHoldem/Values.cs
@@ -2,10 +2,10 @@
 {
     public static class Values
     {
-        public static readonly int Ten = 10;
-        public static readonly int Jack = 11;
-        public static readonly int Queen = 12;
-        public static readonly int King = 13;
-        public static readonly int Ace = 14;
+        public const int Ten = 10;
+        public const int Jack = 11;
+        public const int Queen = 12;
+        public const int King = 13;
+        public const int Ace = 14;
     }
 }

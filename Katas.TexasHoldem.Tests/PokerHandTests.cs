24efd8ac874e4899eb99f4d59d020ce2778277fa
Dennis Somerville <dennis.somerville@gmail.com>
2013-08-09 22:04:49 -0400

    DetectValueGroups

--- Katas.TexasHoldem.Tests/PokerHandTests.cs
+++ Katas.TexasHoldem.Tests/PokerHandTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
@@ -27,6 +28,18 @@ namespace Katas.TexasHoldem.Tests
 
             Assert.That(hand.Cards[0], Is.EqualTo(new Card("4s")));
             Assert.That(hand.Cards[1], Is.EqualTo(new Card("5d")));
+        }
+
+        [Test]
+        [TestCase("4s 4d", 0, 4, 1)]
+        [TestCase("3s 4s 4d", 1, 3, 2)]
+        public void GetValueGroups_GroupsNumbersInDescendingOrderByHighestValue(string handString, int groupNumber, int expectedKey, int expectedGroupCount)
+        {
+            var hand = new PokerHand(handString);
+            var groups = hand.GetValueGroups().ToArray();
+
+            Assert.That(groups[groupNumber].Key, Is.EqualTo(expectedKey));
+            Assert.That(groups.Count(), Is.EqualTo(expectedGroupCount));
         }
 
         
